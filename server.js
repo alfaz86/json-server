4e3068a8df65afe48ae7ded940f42827bd46be5e
alfaz86 <hidecode403@gmail.com>
2023-08-21 16:48:57 +0700

    setting config server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
